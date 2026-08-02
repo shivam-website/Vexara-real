1a7795cb8820c11e06368af2aa14b25900640b6b
Shivam <shivamsah1222@gmail.com>
2026-08-02 09:56:28 +0545

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,18 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker Installed');
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    (async () => {
+      // Clear any caches left by old versions
+      const keys = await caches.keys();
+      await Promise.all(keys.map(k => caches.delete(k)));
+      // Unregister this worker entirely
+      await self.registration.unregister();
+      // Force all open tabs to reload with no service worker controlling them
+      const clientsList = await self.clients.matchAll({ type: 'window' });
+      clientsList.forEach(client => client.navigate(client.url));
+    })()
+  );
 });
